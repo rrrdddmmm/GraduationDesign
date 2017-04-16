9f17884971cc43c56884765a57b3ffcb12e34c56
rrrdddmmm <314187985@qq.com>
2017-04-16 15:14:27 +0800

    修改字段

--- 大创项目/database/project.sql
+++ 大创项目/database/project.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50537
 File Encoding         : 65001
 
-Date: 2017-04-16 03:29:02
+Date: 2017-04-16 15:13:15
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -19,27 +19,32 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `project`;
 CREATE TABLE `project` (
-  `project_no` int(11) NOT NULL,
-  `project_name` varchar(50) NOT NULL,
+  `project_no` int(11) NOT NULL AUTO_INCREMENT,
+  `project_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   `home_no` int(11) DEFAULT NULL,
-  `email` varchar(50) NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
   `project_data` varchar(50) DEFAULT NULL,
-  `project_language` varchar(50) NOT NULL,
-  `develop_platform` varchar(50) NOT NULL,
-  `all_number` int(11) NOT NULL,
-  `pronser_number` int(11) NOT NULL,
-  `crt_time` date NOT NULL,
+  `project_language` varchar(50) DEFAULT NULL,
+  `develop_platform` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `all_number` int(11) DEFAULT NULL,
+  `pronser_number` int(11) DEFAULT NULL,
+  `crt_time` date DEFAULT NULL,
   `update_time` date DEFAULT NULL,
-  `status` int(10) NOT NULL,
-  `leader` varchar(10) NOT NULL,
-  `approver` varchar(10) NOT NULL,
-  `budget` double(10,0) NOT NULL,
-  `describe` varchar(50) NOT NULL,
+  `status` int(10) DEFAULT NULL,
+  `leader` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `approver` varchar(10) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `projectbudget` double(10,0) DEFAULT NULL,
+  `describeproject` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`project_no`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of project
 -- ----------------------------
 INSERT INTO `project` VALUES ('1', '计算机开发', null, '314187985@qq.com', null, 'java', 'windows', '5', '5', '2017-04-13', '2017-04-03', '0', '任博', '宁少杰', '2017', '啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊');
-INSERT INTO `project` VALUES ('2', '会议管理系统', null, '925857350@qq.com', null, 'java', 'windows', '6', '3', '2017-04-16', '2017-04-16', '0', '宁少杰', '任博', '2303', '啊啊啊啊啊啊啊啊啊啊啊');
+INSERT INTO `project` VALUES ('2', '会议管理系统', null, '925857350@qq.com', null, 'java', 'windows', '6', '4', '2017-04-16', '2017-04-16', '0', '宁少杰', '任博', '2303', '啊啊啊啊啊啊啊啊啊啊啊');
+INSERT INTO `project` VALUES ('15', 'first', null, '314187985@qq44.com', null, 'java', 'windows', '4', '0', '2017-04-13', '2017-04-27', '0', 'nisj', 'renbo', '123', 'jisnjsgasdbhsagyuesdsaasd');
+INSERT INTO `project` VALUES ('16', '', null, '314187985@qq44.com', null, '-1', '', null, '0', '2017-04-14', '2017-04-22', '0', '', '', null, '');
+INSERT INTO `project` VALUES ('17', 'sad', null, '', null, 'java', 'asd', '12', '0', '2017-04-22', '2017-04-12', '0', 'sad', 'asd', '211', 'asd');
+INSERT INTO `project` VALUES ('18', 'sad', null, '', null, 'java', 'asd', '12', '0', '2017-04-22', '2017-04-12', '0', 'sad', 'asd', '211', 'asd');
+INSERT INTO `project` VALUES ('19', 'ad', null, '314187985@qq44.com', null, 'java', 'asd', '12', '0', '2017-04-29', '2017-04-18', '0', 'asd', 'sad', '21', 'asd');
