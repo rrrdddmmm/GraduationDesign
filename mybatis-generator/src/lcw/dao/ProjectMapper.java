20ef24b652975db9198bb4c50eb34ff2c07783fa
rrrdddmmm <314187985@qq.com>
2017-04-22 15:05:00 +0800

    项目添加表操作生成器

---
+++ mybatis-generator/src/lcw/dao/ProjectMapper.java
@@ -0,0 +1,17 @@
+package lcw.dao;
+
+import lcw.model.Project;
+
+public interface ProjectMapper {
+    int deleteByPrimaryKey(Integer projectNo);
+
+    int insert(Project record);
+
+    int insertSelective(Project record);
+
+    Project selectByPrimaryKey(Integer projectNo);
+
+    int updateByPrimaryKeySelective(Project record);
+
+    int updateByPrimaryKey(Project record);
+}
